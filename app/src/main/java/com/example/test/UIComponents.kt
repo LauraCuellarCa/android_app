66fb440561f6be857904b123af440afea7692956
Laura Cuellar <laurapcuellar@gmail.com>
2025-04-02 13:23:14 +0200

    added button import

--- app/src/main/java/com/example/test/UIComponents.kt
+++ app/src/main/java/com/example/test/UIComponents.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.foundation.text.BasicTextField
