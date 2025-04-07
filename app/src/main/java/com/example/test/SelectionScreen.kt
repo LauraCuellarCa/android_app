3b6e8cd4265f55847dfbd29b18325e00377f68ec
Paloma Ruiz <palomarm2002@gmail.com>
2025-04-07 11:15:32 +0200

    - interfaz retocada: botón de limpiar en esquina superior derecha, botón micro en el centro título de Escoger tu formulario, más separado

--- app/src/main/java/com/example/test/SelectionScreen.kt
+++ app/src/main/java/com/example/test/SelectionScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
