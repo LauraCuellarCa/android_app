d25d0d7b44e7917a77b9f9509269c39374c7dce3
Laura Cuellar <laurapcuellar@gmail.com>
2025-04-21 10:02:05 +0200

    centering

--- app/src/main/java/com/example/test/WelcomeScreen.kt
+++ app/src/main/java/com/example/test/WelcomeScreen.kt
@@ -45,7 +45,7 @@ fun WelcomeScreen(
             )
             
             // Reduced spacer between logo and text
-            Spacer(modifier = Modifier.height(5.dp))
+            Spacer(modifier = Modifier.height(1.dp))
             
             // Description text
             Text(
